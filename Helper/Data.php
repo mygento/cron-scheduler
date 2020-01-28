776b9ee35c934d070c913621937b2409f5609e79
Dmitrii Chukhnov <d.chuhnov@mygento.ru>
2020-01-28 12:09:51 +0300

    NN2-2162 - added configuration params.

--- Helper/Data.php
+++ Helper/Data.php
@@ -10,8 +10,27 @@ namespace Mygento\CronScheduler\Helper;
 
 class Data extends \Mygento\Base\Helper\Data
 {
+    const XML_PATH_GENERAL_IS_DROP_RUNNING_JOBS = 'cron_scheduler/general/is_drop_running_jobs';
+    const XML_PATH_GENERAL_RUNNING_JOBS_LIMIT = 'cron_scheduler/general/running_jobs_limit';
+
     /**
      * @var string
      */
     protected $code = 'cron_scheduler';
+
+    /**
+     * @return bool
+     */
+    public function isDropRunningJobs()
+    {
+        return $this->scopeConfig->isSetFlag(self::XML_PATH_GENERAL_IS_DROP_RUNNING_JOBS);
+    }
+
+    /**
+     * @return int
+     */
+    public function getRunningJobsLimit()
+    {
+        return (int) $this->scopeConfig->getValue(self::XML_PATH_GENERAL_RUNNING_JOBS_LIMIT);
+    }
 }
